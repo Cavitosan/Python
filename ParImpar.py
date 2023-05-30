17e7ff359d7b3cbaf6977c7f9a4f4c043414d1cc
Carlos Victor Pereira Santana <111398276+Cavitosan@users.noreply.github.com>
2023-05-30 20:16:08 -0300

    Add files via upload

---
+++ ParImpar.py
@@ -0,0 +1,19 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+print ('Python - ChatGpt')
+
+num = int(input("Insira um numero, por gentileza: "))
+
+if (num %2 == 0):
+    print(num, "O número é par")
+else:
+    print(num, "O número é impar")
+    
+quadrado = num ** 0.5
+print("O quadrado da raiz de ", num, " é ", quadrado)
